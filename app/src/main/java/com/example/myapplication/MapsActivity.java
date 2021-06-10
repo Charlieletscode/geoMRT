dbdd2d050ae8cb64f7910c34f8f7e51b08fc9c01
charlie <charliechung88@gmail.com>
2021-06-10 19:04:31 +0800

    10 dropbox pls

--- app/src/main/java/com/example/myapplication/MapsActivity.java
+++ app/src/main/java/com/example/myapplication/MapsActivity.java
@@ -2,7 +2,6 @@ package com.example.myapplication;
 
 import androidx.fragment.app.FragmentActivity;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -16,33 +15,70 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends Activity {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    Button launch, launch1, launch2;
+    String uri;
     private GoogleMap mMap;
-    static final LatLng agra = new LatLng(24.17975090926697, 120.64656943861674);
-    Button launch;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         launch = (Button)findViewById(R.id.btn);
-        launch.setOnClickListener(new View.OnClickListener() {
+        launch.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v) {
+            public void onClick(View v){
+                uri = String.format("google.navigation:q=逢甲大學&mode=d");
+                launchMap();
+            }
+        });
 
+        launch1 = (Button)findViewById(R.id.btn1);
+        launch1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                uri = String.format("google.navigation:q=逢甲大學&mode=w");
+                launchMap();
+            }
+        });
+
+        launch2 = (Button)findViewById(R.id.btn2);
+        launch2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                uri = String.format("google.navigation:q=逢甲大學&mode=b");
                 launchMap();
             }
         });
     }
 
+        public void launchMap(){
+            Uri intentUri = Uri.parse(uri);
+            Intent intent = new Intent(Intent.ACTION_VIEW,intentUri);
+            intent.setPackage("com.google.android.apps.maps");
 
-    public void launchMap(){
-        String uri = String.format("geo:24.17975090926697, 120.64656943861674?q = 台中+逢甲大學");
-        Uri uri1 = Uri.parse(uri);
-        Intent intent = new Intent(Intent.ACTION_VIEW,uri1);
-        intent.setPackage("com.google.android.apps.maps");
 
-        startActivity(intent);
+                startActivity(intent);
+
+        }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
